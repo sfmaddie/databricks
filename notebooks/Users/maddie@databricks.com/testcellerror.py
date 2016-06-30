bb3d2aa0d74a3fe806fa51c27eae25df1b9ff6ca
sfmaddie <sfmaddie@users.noreply.github.com>
2016-06-30 10:40:32 -0700

    rev 1

---
+++ notebooks/Users/maddie@databricks.com/testcellerror.py
@@ -0,0 +1,22 @@
+# Databricks notebook source exported at Thu, 30 Jun 2016 17:40:31 UTC
+for i in range(10):
+    print i
+    import time
+    time.sleep(0.5)
+    if i == 5:
+        raise "ERR!"
+
+# COMMAND ----------
+
+print 1 + 1
+print 2 + 2
+3  * 3
+raise "ERR!"
+5 * 5
+
+# COMMAND ----------
+
+3  * 3
+
+# COMMAND ----------
+
