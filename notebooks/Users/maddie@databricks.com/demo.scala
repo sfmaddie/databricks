9e1880409ddc3c5b0e8e15531229080f2830ff0d
sfmaddie <sfmaddie@users.noreply.github.com>
2016-11-01 15:08:18 -0700

    Update demo.scala

--- notebooks/Users/maddie@databricks.com/demo.scala
+++ notebooks/Users/maddie@databricks.com/demo.scala
@@ -1,5 +1,9 @@
-// Databricks notebook source exported at Mon, 22 Aug 2016 22:16:30 UTC
+// Databricks notebook source exported at Tue, 1 Nov 2016 22:08:18 UTC
 1 + 1
+
+// COMMAND ----------
+
+2 + 2
 
 // COMMAND ----------
 
